5b99b6f2920276af326003647317b5ecefbadf90
CosmicPun <41039@chs.ac.th>
2025-11-20 00:51:20 +0700

    update DAY-5 problems

--- DAY-5/heap_kth/student.h
+++ DAY-5/heap_kth/student.h
@@ -7,8 +7,15 @@
 
 template <typename T,typename Comp >
 T CP::priority_queue<T,Comp>::get_kth(size_t k) const {
-  //write your code here
-  //can include anything
+  std::vector<T> tmp;
+  size_t size;
+  if(mSize > 10)size = 10;
+  else size = mSize;
+  for(size_t i=0;i<size;i++){
+    tmp.push_back(mData[i]);
+  }
+  std::sort(tmp.begin(), tmp.end(), mLess);
+  return tmp[tmp.size()-k];
 }
 
 #endif
